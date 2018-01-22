0174818bb72bd38bec983899e4e031ed29e418da
xelon <alexklatch@gmail.com>
2018-01-22 23:27:32 +0300

    Client updates

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -28,28 +28,57 @@ public class Controller {
     private Socket s;
 
     public void buttonClk(MouseEvent mouseEvent) throws IOException {
-        try {
-            s = new Socket(urlf.getText().toString(), 9351);
-            dbg.appendText("Connected to " + s.getInetAddress() + "\n");
-        } catch (IOException e) {
-            dbg.appendText(("Error: " + e.toString() + "\n"));
-            e.printStackTrace();
-        }
-        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        out.println(term.getText().toString());
-        StringBuilder sb = new StringBuilder(8096);
-        while (true) {
-            if (input.ready()) {
-                int i = 0;
-                while (i != -1) {
-                    i = input.read();
-                    sb.append((char) i);
+        Thread th;
+        th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        s = new Socket(urlf.getText().toString(), 9351);
+                        if (s.isConnected()) System.out.println("Connected");
+                        //dbg.appendText("Connected to " + s.getInetAddress() + "\n");
+                    } catch (IOException e) {
+                        dbg.appendText(("Error: " + e.toString() + "\n"));
+                        e.printStackTrace();
+                    }
+                    PrintWriter out = null;
+                    try {
+                        out = new PrintWriter(s.getOutputStream(), true);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    BufferedReader input = null;
+                    try {
+                        input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    StringBuilder sb = new StringBuilder(8096);
+                    out.println("GET");
+                    while (true) {
+                        try {
+                            if (input.ready()) {
+                                int i = 0;
+                                while (i != -1) {
+                                    i = input.read();
+                                    sb.append((char) i);
+                                }
+                                break;
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(sb.toString());
+                    resp.appendText(sb.toString() + "\n");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-                break;
             }
-        }
-        System.out.println(sb.toString());
-        resp.appendText(sb.toString() + "\n");
+        });
+        th.start();
     }
 }
